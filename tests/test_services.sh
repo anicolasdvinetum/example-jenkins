d18862ef3c91a58384b3de88606143e27b6aa1ba
anicolasdv <alvaro.diaz-valle.ext@inetum.com>
2026-03-23 14:06:25 +0100

    cambiado puerto del servicio go

--- tests/test_services.sh
+++ tests/test_services.sh
@@ -4,7 +4,7 @@ echo "Esperando a que los servicios arranquen..."
 sleep 5
 
 echo "Testeando servicio en Go..."
-curl -f http://go-service:8080 || exit 1
+curl -f http://go-service:9090 || exit 1
 echo "Go OK"
 
 echo "Testeando servicio en Python..."
