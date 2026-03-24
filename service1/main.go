6ed360a824fb477fc5700e2fe5299a2361e5eb35
anicolasdv <alvaro.diaz-valle.ext@inetum.com>
2026-03-24 10:07:25 +0100

    servicios en la misma red que jenkins

--- service1/main.go
+++ service1/main.go
@@ -1,16 +1,16 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"net/http"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Service 1 (Go) OK")
+	fmt.Fprintf(w, "Service 1 (Go) OK")
 }
 
 func main() {
-    http.HandleFunc("/", handler)
-    fmt.Println("Go service running on port 8080...")
-    http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/", handler)
+	fmt.Println("Go service running on port 9090...")
+	http.ListenAndServe(":9090", nil)
 }
